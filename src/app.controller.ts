fcdd95531d586e6c7e9671925ba04ecb5c4298cd
Xarasho <alex.olmedo.rojas@gmail.com>
2024-03-12 19:56:02 -0600

    Implemented the logic of the createReport endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Delete, Get, Post, Put, Param } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { data, ReportType } from './data';
+import { v4 as uuid } from 'uuid';
 
 // Controller entities are classes that allow us to create new endpoints
 @Controller('report/:type') // May have multiple route handlers
@@ -20,9 +29,21 @@ export class AppController {
       .find((report) => report.id === id);
   }
 
-  @Post('')
-  createReport() {
-    return 'created';
+  @Post()
+  createReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: string,
+  ) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+    };
+    data.report.push(newReport);
+    return newReport;
   }
 
   @Put(':id')
